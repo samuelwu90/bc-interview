1b42aa99308a135a48472423c0d07cc433b8a77f
SAMUEL WU <samuelwu90@users.noreply.github.com>
2015-12-14 16:38:21 -0800

    final commit
    - minor refactor of common logic in each route
    - ensure small/medium/large concurrent downloads from s3 are bounded
    (set to 1 now but it’s possible to tune)

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+'use strict';
+
 var async = require('async');
-var _ = require('lodash');
+
 var AWS = require('aws-sdk');
+
 var app = require('express')();
 var archiver = require('archiver');
-var p = require('path');
-var fs = require('fs');
+var moment = require('moment');
 
 var bucket = 'coding-challenges';
 
@@ -19,10 +21,11 @@ app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
-app.get('/small', function (req, res) {
+var s3ToZip = function s3ToZip(prefix, filename, req, res){
 
+    var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+    var startTime = new moment();
     var archive = archiver('zip');
-    var prefix = 'file_archiving/small_project/';
 
     archive.on('error', function(err) {
         res.status(500).send({error: err.message});
@@ -31,13 +34,12 @@ app.get('/small', function (req, res) {
     //on stream closed we can end the request
     archive.on('end', function() {
         console.log('Archive wrote %d bytes', archive.pointer());
+        console.log('Took: ', moment().diff(startTime), ' ms');
     });
 
-    res.attachment('small_project.zip');
+    res.attachment(filename);
 
     archive.pipe(res);
-
-    var s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
     var params = {
       Bucket: bucket,
@@ -45,89 +47,51 @@ app.get('/small', function (req, res) {
     };
 
     s3.listObjects(params, function(err, data){
-        _.each(data.Contents, function(object){
-            archive.append(s3.getObject({Bucket: bucket, Key: object.Key}).createReadStream(), { name: object.Key.replace(prefix, '') });
-        })
-        archive.finalize();
-    })
+
+        async.eachLimit(data.Contents, 1, function(object, cb){
+            var stream = s3.getObject({Bucket: bucket, Key: object.Key}).createReadStream();
+
+            stream.on('end', function(){
+                console.log('finished streaming: ' + object.Key);
+                return cb();
+            });
+
+            archive.append(stream, { name: object.Key.replace(prefix, '') });
+        }, function(err){
+            if(err) return res.status(500).send({error: err.message});
+            archive.finalize();
+        });
+
+    });
+}
+
+app.get('/small', function (req, res) {
+
+    var prefix = 'file_archiving/small_project/';
+    var filename = 'small_project.zip';
+    return s3ToZip(prefix, filename, req, res);
 
 });
 
 app.get('/medium', function (req, res) {
 
-    var archive = archiver('zip');
     var prefix = 'file_archiving/medium_project/';
-
-    archive.on('error', function(err) {
-        res.status(500).send({error: err.message});
-    });
-
-    //on stream closed we can end the request
-    archive.on('end', function() {
-        console.log('Archive wrote %d bytes', archive.pointer());
-    });
-
-    res.attachment('medium_project.zip');
-
-    archive.pipe(res);
-
-    var s3 = new AWS.S3({apiVersion: '2006-03-01'});
-
-    var params = {
-      Bucket: bucket,
-      Prefix: prefix
-    };
-
-    s3.listObjects(params, function(err, data){
-        _.each(data.Contents, function(object){
-            archive.append(s3.getObject({Bucket: bucket, Key: object.Key}).createReadStream(), { name: object.Key.replace(prefix, '') });
-        })
-        archive.finalize();
-    })
+    var filename = 'medium_project.zip';
+    return s3ToZip(prefix, filename, req, res);
 
 });
 
 app.get('/large', function (req, res) {
 
-    var archive = archiver('zip');
     var prefix = 'file_archiving/large_project/';
-
-    archive.on('error', function(err) {
-        res.status(500).send({error: err.message});
-    });
-
-    //on stream closed we can end the request
-    archive.on('end', function() {
-        console.log('Archive wrote %d bytes', archive.pointer());
-    });
-
-    res.attachment('large_project.zip');
-
-    archive.pipe(res);
-
-    var s3 = new AWS.S3({apiVersion: '2006-03-01'});
-
-    var params = {
-      Bucket: bucket,
-      Prefix: prefix
-    };
-
-    s3.listObjects(params, function(err, data){
-        console.log(data.Contents.length)
-        async.eachLimit(data.Contents, 10, function(object, cb){
-            var stream = s3.getObject({Bucket: bucket, Key: object.Key}).createReadStream();
-
-            stream.on('end', function(){
-                console.log('finished streaming: ' + object.Key)
-                return cb();
-            })
-
-            archive.append(stream, { name: object.Key.replace(prefix, '') });
-        }, function(err){
-            archive.finalize();
-        })
-    })
+    var filename = 'large_project.zip';
+    return s3ToZip(prefix, filename, req, res);
 
 });
+
+
+// setInterval(function(){
+//     console.log(util.inspect(process.memoryUsage()));
+// }, 200)
 
 app.listen(3000);
